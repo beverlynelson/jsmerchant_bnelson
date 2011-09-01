41900a4dc62db38eab3530346053f28a54ee7108
Jeff Casimir <jeff@casimircreative.com>
2011-08-31 23:24:27 -0400

    Set order_item quantity to 0 when intializing, then increment whether initializing or found existing

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,7 +3,8 @@ class OrderItemsController < ApplicationController
   
   def create
     #@order_item = @order.order_items.new(:quantity =>1, :product_id => params[:product_id])
-    @order_item = @order.order_items.find_or_initialize_by_product_id(params[:product_id])
+    @order_item = @order.order_items.find_or_initialize_by_product_id(params[:product_id], :quantity => 0)
+    @order_item.quantity += 1
     if @order_item.save
       redirect_to @order, :notice => "Successfully created order item."
     else
