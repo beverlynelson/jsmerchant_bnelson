de7a7a6f29662d63a1c605e33158f31b0fd7b2c0
Jeff Casimir <jeff@casimircreative.com>
2011-08-31 23:22:05 -0400

    Change the redirect to point to the order

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,7 +5,7 @@ class OrderItemsController < ApplicationController
     #@order_item = @order.order_items.new(:quantity =>1, :product_id => params[:product_id])
     @order_item = @order.order_items.find_or_initialize_by_product_id(params[:product_id])
     if @order_item.save
-      redirect_to @order_item, :notice => "Successfully created order item."
+      redirect_to @order, :notice => "Successfully created order item."
     else
       render :action => 'new'
     end
